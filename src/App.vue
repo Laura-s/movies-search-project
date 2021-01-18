6e624a214db005f65c86c2bd686ed8afac8c6b7d
Laura <laura.sfichi@yahoo.ro>
2021-01-18 21:49:05 +0200

    refactor vue components

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,104 @@
 <template>
   <div class="container">
-  <div class="row justify-content-center py-5" id="search-row">
-    <div class="col-5">
-      <div class="input-group ">
-        <input type="text" id="input-title" class="form-control" placeholder="Title" aria-label="Title" aria-describedby="button-addon2" @keyup="buttonReact" ref="input" @keydown="getResults">
-        <button class="btn btn-outline-primary disabled" type="button" id="button-addon2" ref="button" @click="getResults">
-          <span class="spinner-border spinner-border-sm " role="status" aria-hidden="true"></span>
-          Search</button>
+    <div class="row justify-content-center py-5" id="search-row">
+      <div class="col-5">
+        <div class="input-group">
+          <input
+            type="text"
+            id="input-title"
+            class="form-control"
+            placeholder="Title"
+            aria-label="Title"
+            aria-describedby="button-addon2"
+            @keydown.enter="getResults"
+            v-model="searchTerm"
+          />
+          <button
+            class="btn btn-outline-primary"
+            type="button"
+            id="button-addon2"
+            v-bind:class="{
+              loading: isLoading,
+              disabled: searchTerm.trim() === '',
+            }"
+            @click="getResults"
+          >
+            <span
+              class="spinner-border spinner-border-sm"
+              role="status"
+              aria-hidden="true"
+            ></span>
+            Search
+          </button>
+        </div>
       </div>
     </div>
+    <div class="row" id="results-row">
+      <MovieCard v-for="movie in movies" :key="movie.id" :movie="movie" />
+    </div>
   </div>
-  <div class="row" id="results-row" ref="row"></div>
-</div>
 </template>
 
 <script>
+import MovieCard from "./components/movie-card";
 export default {
+  data: () => ({
+    isLoading: false,
+    searchTerm: "",
+    movies: [],
+  }),
   methods: {
-    getResults(){
-      const url = 'https://imdb8.p.rapidapi.com/title/find?q='+ this.$refs.input.value.trim();
-      const options = {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "4e037fb5a4mshd017cdd280e6974p11ccbdjsna16daae232bc",
-            "x-rapidapi-host": "imdb8.p.rapidapi.com"
-        }
+    getResults() {
+      if (this.searchTerm.trim() !== "") {
+        const url =
+          "https://imdb8.p.rapidapi.com/title/find?q=" + this.searchTerm.trim();
+        const options = {
+          method: "GET",
+          headers: {
+            "x-rapidapi-key":
+              "4e037fb5a4mshd017cdd280e6974p11ccbdjsna16daae232bc",
+            "x-rapidapi-host": "imdb8.p.rapidapi.com",
+          },
+        };
+        this.isLoading = true;
+
+        fetch(url, options)
+          .then((response) => response.json())
+          .then((data) => {
+            this.isLoading = false;
+            this.movies = data.results.filter(
+              (movie) => movie.titleType == "movie"
+            );
+          });
       }
-      this.$refs.button.classList.add('loading');
-
-      fetch(url, options)
-      .then(response => response.json())
-      .then(data => {
-        this.$refs.button.classList.remove('loading');
-
-        this.$refs.row.innerHTML = ''
-
-        data.results.filter(movie => movie.titleType == "movie").forEach( movie => {
-          const imgNotFoundSrc = 'https://cdn.browshot.com/static/images/not-found.png';
-          const imgSrc = movie.image && movie.image.url ? movie.image.url : imgNotFoundSrc;
-          const movieYear = movie.year || 'n/a';
-          const colHtml = `
-            <div class="card">
-                <img src="${imgSrc}" class="card-img-top" alt="...">
-                <div class="card-body">
-                <h5 class="card-title">${movie.title}</h5>
-                <p class="card-text">${movieYear}</p>
-                </div>
-            </div>`
-        
-          const colon = document.createElement('DIV')
-          colon.innerHTML = colHtml;
-          colon.classList.add('col-3', 'pb-4');
-          this.$refs.row.appendChild(colon);
-        });
-        
-      });
     },
-
-    buttonReact(e){
-      if (e.target.value.trim() !== ""){
-        this.$refs.button.classList.remove('disabled')
-      }else{
-        this.$refs.button.classList.add('disabled')
-      }
-    }
-  }
-}
+  },
+  components: {
+    MovieCard,
+  },
+};
 </script>
 
 <style>
-#search-row .spinner-border{
+#search-row .spinner-border {
   display: none;
 }
 
-#search-row .loading .spinner-border{
+#search-row .loading .spinner-border {
   display: inline-block;
-
 }
 
-.card-body .card-title{
+.card-body .card-title {
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
 }
 
-.card img{
+.card img {
   height: 300px;
   object-fit: cover;
 }
-#button-addon2{
+#button-addon2 {
   width: 100px;
 }
 </style>
---
+++ src/components/movie-card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="col-3 pb-4">
+    <div class="card">
+      <img
+        :src="movie.image && movie.image.url ? movie.image.url : imgNotFoundSrc"
+        class="card-img-top"
+        alt="..."
+      />
+      <div class="card-body">
+        <h5 class="card-title">
+          {{movie.title}}
+        </h5>
+        <p class="card-text">{{movie.year || "n/a"}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['movie'],
+    data:() =>({
+        imgNotFoundSrc : "https://cdn.browshot.com/static/images/not-found.png"
+    })
+};
+</script>
+
+<style>
+</style>
